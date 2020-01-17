9c5fa43f8037d0cf326054dcb6aaf47be327a7a6
NAWEEN0536 <59818716+NAWEEN0536@users.noreply.github.com>
2020-01-17 14:37:30 +0530

    Calc 2

--- calc.js
+++ calc.js
@@ -6,4 +6,32 @@ function add()
      console.log(sum);
 
 }
-add(11,1,2,7);
+add();
+
+
+function add()
+{
+var n1=10,n2=12;
+sum=(n1+n2);
+console.log(sum);
+}
+add();
+
+
+
+function add()
+{
+var n1=54,n2=75;
+sub=(n1-n2);
+console.log(sub);
+}
+add();
+
+
+function add()
+{
+var n1=8,n2=6,n3=4;
+sub=(n1-n2-n3);
+console.log(sub);
+}
+add();
