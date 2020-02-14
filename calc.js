2d958817660c81f20956054013f9281a2d3fb608
ankit0537 <59608445+ankit0537@users.noreply.github.com>
2020-02-15 00:28:10 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,46 +1,19 @@
-function add()
-
-{
-     var n1=2,n2=3,n3,n4;
-     sum=(n1+n2+n3);
-     console.log(sum);
-
-}
-add();
-
-
-function add()
-{
-var n1=10,n2=12;
-sum=(n1+n2);
-console.log(sum);
-}
-add();
-
-
-
-function add()
-{
-var n1=54,n2=75;
-sub=(n1-n2);
-console.log(sub);
-}
-add();
-
-
-function add()
-{
-var n1=8,n2=6,n3=4;
-sub=(n1-n2-n3);
-console.log(sub);
-}
-add();
-
-
-function add()
-{
-var n1=2,n2=3;
-mul=(n1*n2);
-console.log();
-}
-add();
+class calc{
+      add(n1,n2){
+           return n1+n2;
+      }
+      sub(n1,n2){
+           return  n1+n2;
+      }
+     div(n1,n2){
+           return  n1+n2;
+      }
+     mul(n1,n2){
+           return  n1+n2;
+      }
+     
+     var obj= new calc();
+     console.log(obj.add(10,10));
+     console.log(obj.sub(10,10));
+     console.log(obj.div(10,10));
+     console.log(obj.mul(10,10));
